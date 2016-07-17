2fa2d845d754d40de8c1e9b35b0d731a7ebad70a
K. Mannah Kallon <kenikall@gmail.com>
2016-07-17 15:46:28 -0700

    Complete data validation and logic tree.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,27 +1,37 @@
+def is_number?(obj)
+    return obj.to_s == obj.to_i.to_s
+end
+
+ageValidinput = false
 birthValidinput = false
 garlicValidinput = false
+healthValidinput = false
 
 puts "What is your name?"
 name = gets.chomp
-puts "How old are you?"
-age = gets.chomp
-puts "What year were you born"
-birthYear = gets.chomp
 
-until garlicValidinput
-  puts "Have you had your birthday this year?"
-birthdayThisYear = gets.chomp
-  if birthdayThisYear == "y" || birthdayThisYear == "yes" || birthdayThisYear == "Y" || birthdayThisYear == "Yes"
-    gBread = true
-    garlicValidinput = true
-  elsif birthdayThisYear == "n" || birthdayThisYear == "no" || birthdayThisYear == "N" || birthdayThisYear == "No"
-    gBread = false
-    garlicValidinput = true
-  else
-    puts "Please answer yes or no."
-  end
+until ageValidinput
+	puts "How old are you?"
+	age = gets.chomp
+	if is_number?(age) 
+		ageValidinput = true
+	else
+		puts "Please enter an interger."
+	end
 end
+age = age.to_i
 
+until birthValidinput
+	puts "What year were you born"
+	birthYear = gets.chomp
+	if is_number?(birthYear) 
+		birthValidinput = true
+	else
+		puts "Please enter an interger."
+	end
+end
+realAge = 2016 - birthYear.to_i
+realAge = realAge.to_i
 
 until garlicValidinput
   puts "Should we save you some garlic bread?"
@@ -36,5 +46,38 @@ until garlicValidinput
     puts "Please answer yes or no."
   end
 end
-puts "Would you like to enroll in health insurance?"
-hInsurance = gets.chomp
+
+until healthValidinput
+	puts "Would you like to enroll in health insurance?"
+	hInsurance = gets.chomp	
+	
+	if hInsurance == "y" || hInsurance == "yes" || hInsurance == "Y" || hInsurance == "Yes"
+    	hInsurance = true
+    	healthValidinput = true
+  	elsif hInsurance == "n" || hInsurance == "no" || hInsurance == "N" || hInsurance == "No"
+    	hInsurance = false
+    	healthValidinput = true
+  	else
+    	puts "Please answer yes or no."
+  	end
+end
+
+
+if name == "Drake Cula" ||name == "Tu Fang"
+	p "Definitely a vampire."
+elsif realAge == age
+	if gBread || hInsurance
+		p "Probably not a vampire."
+	else
+		p "Results inconclusive."
+	end 
+elsif realAge != age 
+	if gBread || hInsurance
+		p "Probably a vampire."
+	else 
+		p "Almost certainly a vampire."
+	end
+else
+	p "Results inconclusive."
+end
+	
