c3cf481f337bf6622d589ac1d7fa59748bfc7c68
K. Mannah Kallon <kenikall@gmail.com>
2016-07-17 14:23:04 -0700

    Add initial questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born"
+birthYear = gets.chomp
+puts "Should we save you some garlic bread?"
+gBread = gets.chomp
+puts "Would you like to enroll in health insurance?"
+hInsurance = gets.chomp
