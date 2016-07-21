11e422ab7ddac12bebc8a611a5b2f944d9de295a
K. Mannah Kallon <kenikall@gmail.com>
2016-07-20 18:24:09 -0700

    Debug data validation

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,16 +3,50 @@ customer = {}
 
 puts "What is the customer's name?"
 customer[:name] =gets.chomp.split
-p customer[:name] 
+puts "Mr., Mrs., Ms., or other?"
+customer[:honorific] = gets.chomp
+	if customer[:honorific].downcase.strip == "mr" || customer[:honorific].downcase.strip == "mr."
+		customer[:honorific] ="Mr."
+	elsif customer[:honorific].downcase.strip == "mrs" || customer[:honorific].downcase.strip == "mrs."
+		customer[:honorific] ="Mrs."		
+	elsif customer[:honorific].downcase.strip == "ms" || customer[:honorific].downcase.strip == "ms."
+		customer[:honorific] = "Ms."
+	else
+	validhonorific = false	
+		until validhonorific
+			puts "What honorific would you like?"
+			customer[:honorific] = gets.chomp
+			puts "Is #{customer[:honorific]} the honorific you would like?"
+			yn = gets.chomp
+			if yn[0].downcase == "y"
+				validhonorific = true
+			end
+		end
+	end
+puts "Thank you #{customer[:honorific]} #{customer[:name][-1]}"
 
-puts "What is the customer's street?"
-	address = { name: customer[:name]}
-	address[:street] = gets.chomp
-puts "What is the customer's city?"
-	address[:city] = gets.chomp
-puts "What is the customer's state?"
-	address[:state] = gets.chomp
-puts "What is the customer's zipcode"
-	address[:zip] = gets.chomp
-customer[:address] = address
-p customer[:address][:city]
+validaddress = false
+until validaddress 
+	puts "What is #{customer[:honorific]} #{customer[:name][-1]}'s address?"
+		address = { name: customer[:name]}
+	puts "Street?"
+		address[:street] = gets.chomp
+	puts "City?"
+		address[:city] = gets.chomp
+	puts "State?"
+		address[:state] = gets.chomp
+	puts "Zipcode?"
+		address[:zip] = gets.chomp
+	customer[:address] = address
+
+	puts "#{customer[:honorific]} #{customer[:name][-1]}'s mailing address is:"
+	puts "#{customer[:name].join.to_s}"
+	puts "#{customer[:address][:street]}"
+	puts "#{customer[:address][:city]}, #{customer[:address][:state]}  #{customer[:address][:zip]} "
+	puts "Is this correct?"
+
+	yn = gets.chomp
+	if yn[0].downcase == "y"
+		validaddress = true
+	end
+end
