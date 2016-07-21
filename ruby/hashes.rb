aaa699913cf3b9890bad8a4bcb06da50df0496da
K. Mannah Kallon <kenikall@gmail.com>
2016-07-20 19:44:51 -0700

    Complete challenge

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -14,9 +14,14 @@ customer[:honorific] = gets.chomp
 	else
 	validhonorific = false	
 		until validhonorific
-			puts "What honorific would you like?"
+			if customer[:honorific] == "other"
+				puts "What honorific would you like?"
+			else
+				puts "I don't recognize that. What honorific would you like?"
+			end
 			customer[:honorific] = gets.chomp
-			puts "Is #{customer[:honorific]} the honorific you would like?"
+			puts "Does #{customer[:honorific]} work for you?"
+			customer[:honorific] == "other"
 			yn = gets.chomp
 			if yn[0].downcase == "y"
 				validhonorific = true
@@ -40,7 +45,7 @@ until validaddress
 	customer[:address] = address
 
 	puts "#{customer[:honorific]} #{customer[:name][-1]}'s mailing address is:"
-	puts "#{customer[:name].join.to_s}"
+	puts "#{customer[:name].join(" ").to_s}"
 	puts "#{customer[:address][:street]}"
 
 	puts "#{customer[:address][:city]}, #{customer[:address][:state]}  #{customer[:address][:zip]} "
@@ -64,8 +69,9 @@ styles = {
 	stylish: false
 }
 validstyles = false
-#until validstyles
-	puts "Chose the numbers that describe your dream home"
+until validstyles
+	badinput = false
+	puts "Choose the numbers that describe your dream home"
 	puts "  1. cozy       2. light         3. charming"
 	puts "  4. classic    5. comfortable   6. distinctive"
 	puts "  7. glamorous  8. luxurious     9. stylish"
@@ -73,33 +79,47 @@ validstyles = false
 	customer[:styles] = gets.chomp.split
 
 	customer[:styles].each{ |x|
-		if x == "1"
+		if x[0][0] == "1"
 			styles[:cozy] = true
-		elsif x == "2"
+		elsif x[0][0] == "2"
 			styles[:light] = true
-		elsif x == "3"
+		elsif x[0][0] == "3"
 			styles[:charming] = true
-		elsif x == "4"
+		elsif x[0][0] == "4"
 			styles[:classic] = true
-		elsif x == "5"
+		elsif x[0][0] == "5"
 			styles[:comfortable] = true
-		elsif x == "6"
+		elsif x[0][0] == "6"
 			styles[:distinctive] = true
-		elsif x == "7"
+		elsif x[0][0] == "7"
 			styles[:glamorous] = true
-		elsif x == "8"
+		elsif x[0][0] == "8"
 			styles[:luxurious] = true
-		elsif x == "9"
+		elsif x[0][0] == "9"
 			styles[:stylish] = true
 		else
 			puts "#{x} was not recognized. Pease enter only numers 1 - 9"
+			badinput = true
+			break
 		end
  		}
 	ary = []
-	styles.each_value{|x| 
-		if x 
-			ary << key
-		end
+	styles.each_key{|x| 
+		if styles[x] 
+			ary << x.to_s
+		 end
 	}
- 	puts "Your dream home will be #{ary.join(", ").to_s}."
-#end
+	if badinput
+ 	else 
+	 	puts "Your dream home will be #{ary.join(", ").to_s}."
+ 		puts "Are you satisfied with that description?"
+ 		yn = gets.chomp
+ 		if yn[0].downcase == "y"
+ 			validstyles = true
+ 			customer[:styles]=ary
+ 		else
+ 			puts"Please choose your descriptors again."
+ 		end
+ 	end
+end
+p customer
