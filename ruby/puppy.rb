bb088c3c72fad260affab058e35e2e717614bdaf
K. Mannah Kallon <kenikall@gmail.com>
2016-07-25 15:04:01 -0700

    Create puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ----------------------
+# varies
+
+# CHARACTERISTICS --------------
+# weight: varies
+# hair length: varies
+# color: varies
+
+# BEHAVIOR ---------------------
+# fetch
+# chew
+# obey
+
+class Puppy
+end
+
+p Puppy.methods 
