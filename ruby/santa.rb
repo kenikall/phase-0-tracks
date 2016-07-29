9c72de3cbed16c208384d139c2aa02fd4ba5381d
K. Mannah Kallon <kenikall@gmail.com>
2016-07-29 16:54:54 -0700

    Complete challenge.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,8 @@
 class Santa
+attr_reader :gender, :reindeer
+attr_accessor :ethnicity, :age 
 
 	def initialize (gender, ethnicity)
-		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
@@ -21,22 +22,23 @@ class Santa
 	end
 	
 	def celebrate_birthday
+		puts @age
 		return @age += 1
 	end
 
 	def get_mad_at(reindeer)
-		@reindeer_ranking.remove(reindeer)
+		@reindeer_ranking.delete(reindeer)
 		return @reindeer_ranking<<reindeer
 	end
 
-	#GETTERS
-	def age
-		return @age
-	end
+	#GETTERS that we no longer need
+	# def age
+	# 	return @age
+	# end
 
-	def ethnicity
-		return @ethnicity
-	end 
+	# def ethnicity
+	# 	return @ethnicity
+	# end 
 
 	#SETTERS
 end
@@ -50,13 +52,21 @@ santas = []
 reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+cookies = ["Chocolate Chips", "Oatmeal Raisin", "Chocolate Decadence", "Peanut Butter", "Peanut Butter Milk Chocolate"]
+1.times do |i|
+  santas << Santa.new(example_genders[rand(6)], example_ethnicities[rand(6)])
 end
 
 santas.length.times{|x| 
-	rand(20).times {santas[x].celebrate_birthday}
-	santas[x].id_santa 
-	puts "I am #{santas[x].age} old"
+	rand(140).times {santas[x].celebrate_birthday}
+	puts "I am a #{santas[x].ethnicity}, #{santas[x].gender} santa who is #{santas[x].age} years old"
+	if santas[x].age>100
+		rand_reindeer=reindeer[rand(8)]
+		puts "I'm mad at #{rand_reindeer} my favorites are now #{santas[x].get_mad_at(rand_reindeer)}"
+	elsif santas[x].age<=20
+		santa[x].speak
+	elsif santas[x].age>40 &&santas[x].age<80
+		santas[x].eat_milk_and_cookies(cookies[rand(4)])
+	end
 }
 	
