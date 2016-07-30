6a721562893a2d2da90f70e3b96488d3c3b4d58a
K. Mannah Kallon <kenikall@gmail.com>
2016-07-30 14:01:15 -0700

    Create initilaize and setup methods.

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,21 +1,34 @@
 class Hangman
 	#make array of guesses readable and writable by player
 	def initialize(difficulty)
-		#arrays for easy medium and hard words
-		#set word for the game
-		#make wrong couter = 0
+		@wrong_ctr = 0 #make wrong couter = 0
+		@word = {} # make an empty hash that will represent the word
 		#make won state false
 		#make lost state false
+		setup(difficulty) #setup game with random word
 	end
 
-	def setup
-		#takes word from word list and makes it a hash with each key 
-		#representing a letter and each value either being 
-		#guessed or not guessed
-	end
+	def setup(difficulty)
+		#arrays for easy medium and hard words
+		easy = ["toy", "zoo", "calm","cast","doll","film","fort","kids","mill","nuts","rush","sale","slip","soap","spin","trap","tune","wolf"]
+		medium = ["acres","adult","brick","canal","chose","claws","coach","depth","exist","folks","habit","image","label","lungs","mount","pride","rhyme","rocky","shake","shout","silly","slope","solar","stiff","swung","tales","thumb","vapor","advice"]
+		hard = ["mission", "require","shaking","shallow","species","tobacco","treated","vessels","contrast","exchange","floating","official","positive","possibly","promised","simplest","essential","fireplace","ourselves","policeman","practical","satisfied","selection","constantly","discussion","mysterious","remarkable","satellites","university","arrangement","explanation","grandmother","independent","mathematics","neighborhood","occasionally","relationship","manufacturing"]		#set word for the game
+		#takes random word from word list and splits it into an array of letters
+		if difficulty == "easy"
+			letters = easy[rand(easy.length-1)].chars
+		elsif difficulty == "medium"
+			letters = medium[rand(medium.length-1)].chars
+		elsif difficulty == "hard"
+			letters = hard[rand(hard.length-1)].chars
+		end
+
+		#makes word a hash with each key representing a letter and each value of false to indicate the letter has not been guessed
+		letters.each{|x| @word[x] = false}
+		p letters.join
+		p @word
+	end 
 
 	def progress
-		if 
 		#shows how close the man is to being hung
 		#shows incorrect choices
 	end
@@ -31,3 +44,6 @@ class Hangman
 		#prints losing message and game data
 	end 
 end
+
+#DRIVER CODE
+game = Hangman.new("hard")
---
+++ ruby/hangman/hangman_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'hangman'
+
+describe Hangman do
+  let(:hangman) { Hangman.new }
+
+  it "adds two integers" do
+    expect(calculator.add(3,4)).to eq 7
+  end
+
+  it "subtracts two integers" do
+    expect(calculator.subtract(7,1)).to eq 6
+  end
+
+  it "multiplies two integers" do
+    expect(calculator.multiply(2,3)).to eq 6
+  end
+end
