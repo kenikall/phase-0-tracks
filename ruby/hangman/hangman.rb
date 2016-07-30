192e43fee7a9d23cb2af90bd45cbd21551814d46
K. Mannah Kallon <kenikall@gmail.com>
2016-07-30 12:21:27 -0700

    Create and comment hangman and hangman spec file

---
+++ ruby/hangman/hangman.rb
@@ -0,0 +1,33 @@
+class Hangman
+	#make array of guesses readable and writable by player
+	def initialize(difficulty)
+		#arrays for easy medium and hard words
+		#set word for the game
+		#make wrong couter = 0
+		#make won state false
+		#make lost state false
+	end
+
+	def setup
+		#takes word from word list and makes it a hash with each key 
+		#representing a letter and each value either being 
+		#guessed or not guessed
+	end
+
+	def progress
+		if 
+		#shows how close the man is to being hung
+		#shows incorrect choices
+	end
+
+	def check(guess)
+		#checks to see if guess is in the word
+		#prints either __ or correct letters
+	end
+
+	def gamestate
+		#checks to see if game is won or lost
+		#prints winning message and game data
+		#prints losing message and game data
+	end 
+end
