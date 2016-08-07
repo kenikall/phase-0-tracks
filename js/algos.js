b56dcf0f41c122537a6f006367e8aa7917e89e21
K. Mannah Kallon <kenikall@gmail.com>
2016-08-07 10:43:58 -0700

    Psudocode write_strings.

--- js/algos.js
+++ js/algos.js
@@ -11,15 +11,6 @@ function longest_word(ary)//take in array
 	return(longest);//after looping through the array return baseline
 }
 
-// In algos.js, write a function that takes two objects and checks to see if the objects share at least one key-value pair. 
-// (You'll keep adding to this file, so we suggest grouping functions at the top and testing at the bottom of the file.) 
-// If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, the function would return true, 
-// because at least one key-value pair matches between the two objects. If no pairs match 
-// (and keep in mind that the two objects may not even have any of the same keys), the function should return false. 
-// To make your life easier, don't worry about whether a property is a string ('age') or an identifier name (age). 
-// Those can be considered equivalent. Again, try to reason through the problem using the basics you've already learned, 
-// rather than looking up slick search functions that will do the job for you. We'd rather see you write code that you actually understand!
-
 function key_value_check (test1, test2) //take in 2 objects
 {
 	var test1_keys = Object.keys(test1); //make an array of the keys in the first argument
@@ -40,6 +31,26 @@ function key_value_check (test1, test2) //take in 2 objects
 	}
 	return(false); //if you are done looping and there are no matches return false
 }
+
+// Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
+// So if we ran your function with an argument of 3, we would get an array of 3 random words back 
+// (the words don't have to be actual sensical English words -- "nnnnfph" totally counts). The words should be of randomly 
+// varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, 
+// 	that you'll have to look up, but the solution on how to do so is relatively straightforward.)
+// Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word"
+// function, and prints the result.
+
+functon write_strings(how_long)
+{
+	//make empty array
+	//loop based on function input
+	//create random word
+	//put random word into array
+
+	//return array
+}
+
+
 
 //DRIVER CODE
 console.log(longest_word(["0", "00", "000", "0000"]));
