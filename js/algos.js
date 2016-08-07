ddb3698131078002e117cd40440f3e24dc76bfc8
K. Mannah Kallon <kenikall@gmail.com>
2016-08-07 09:54:59 -0700

    Write psudocode.

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+//take in array
+//initialize a baseline length of 0
+//loop through the array starting at the beginging
+//find the lenght of current element
+//if the length of the current elemement is longer than the baseline make the current element the baseline 
+//after looping through the array return baseline
+
+longest_word(["long phrase","longest phrase","longer phrase"]);
