bc0866e07221978d44fd6182639c541f84eef5b0
K. Mannah Kallon <kenikall@gmail.com>
2016-08-18 12:35:05 -0700

    Complete challenge.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -8,9 +8,14 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+db.execute("CREATE TABLE IF NOT EXISTS campus (
+	id INTERGER PRIMART KEY,
+	short_name VARCHAR(255),
+	long_name VARCHAR(255))")
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @campus = db.execute("SELECT * FROM campus")
   erb :home
 end
 
@@ -23,11 +28,17 @@ get '/students/new' do
   erb :new_student
 end
 
-# create new students via
-# a form
+get '/campus/new' do
+  erb :new_campus
+end
+# create new students via a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
-# add static resources
+post '/campus' do
+  db.execute("INSERT INTO campus (short_name, long_name) VALUES (?,?)", [params['short_name'], params['long_name']])
+  redirect '/'
+end
+#add static resources
